b3db9ff7fed4fe9f4630f8fcba1e0266a0393842
Emil <emilpradeep@gmail.com>
2025-06-09 22:08:57 +0530

    initial commit

---
+++ src/main/java/com/xperia/xpense_scheduler/XpenseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.xperia.xpense_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpenseSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XpenseSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xperia/xpense_scheduler/XpenseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xperia.xpense_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpenseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
